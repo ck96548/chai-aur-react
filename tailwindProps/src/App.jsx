489f6814d8b7b377cedb8f3bc324b1716396b23c
ck96548 <ck96548@gmail.com>
2024-04-11 12:26:21 +0530

    Props and tailwinds

---
+++ tailwindProps/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Card from './componenets/Card'
+
+function App() {
+  
+
+  return (
+    <>
+   <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>TailWind test</h1>
+   <Card userName ="Chai code"/>
+   <Card userName ="Coffe and Code"/>
+   
+  
+    </>
+  )
+}
+
+export default App
